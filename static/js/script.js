0966eed7a52e3b6efc01ffb836740d9e1decccae
terriblesaint <144727084+terriblesaint@users.noreply.github.com>
2024-02-29 12:41:09 -0500

    html formatting cleanup bootstrap table implement ui cleanup

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,39 @@ $(document).ready(function() {
     $(".flash-message .close").on("click", function() {
         $(this).parent().fadeOut('slow');
     });
+
+    //  credit to https://examples.bootstrap-table.com/#welcome.html
+  var $table = $('#table')
+  var $remove = $('#remove')
+  var selections = []
+
+  function initTable() {
+    $table.bootstrapTable('destroy').bootstrapTable({
+      height: 1000,
+      locale: $('#locale').val(),
+    })
+    $table.on('check.bs.table uncheck.bs.table ' +
+      'check-all.bs.table uncheck-all.bs.table',
+    function () {
+      $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+
+    })
+
+    $remove.click(function () {
+      var ids = getIdSelections()
+      $table.bootstrapTable('remove', {
+        field: 'id',
+        values: ids
+      })
+      $remove.prop('disabled', true)
+    })
+  }
+
+  $(function() {
+    initTable()
+
+    $('#locale').change(initTable)
+  })
+
+
 });
